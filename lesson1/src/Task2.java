55442e300f3e9aa5ea77ac4865c7a833de14e8a0
Georgiy <georgii_sidenko@mail.ru>
2023-06-06 18:29:08 +0300

    2 task

---
+++ lesson1/src/Task2.java
@@ -0,0 +1,28 @@
+// 2) Вывести все простые числа от 1 до 1000
+
+import java.util.Scanner;
+import java.io.*;
+
+public class Task2 {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите число:");
+        int number = scanner.nextInt();
+        scanner.close();
+        System.out.println("Простые числа до числа " + number + ":");
+        for (int i = 2; i <= number; i++) {
+            boolean isPrimeNumber = true;
+            for (int j = 2; j < i; j++) {
+                if (i % j == 0) {
+                    isPrimeNumber = false;
+                    break;
+                }
+            }
+
+        if(isPrimeNumber){
+            System.out.print(i + ", ");
+            }
+        }
+    System.out.println();
+    }
+}
