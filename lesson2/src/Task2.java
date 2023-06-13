8197408ce4778943a4447fbf1b2ad61bd9010bee
georgiy <georgii_sidenko@mail.ru>
2023-06-13 17:56:16 +0300

    add logging

--- lesson2/src/Task2.java
+++ lesson2/src/Task2.java
@@ -1,9 +1,28 @@
 // 2) Реализуйте алгоритм сортировки пузырьком числового массива, 
 // результат после каждой итерации запишите в лог-файл.
 
-public class Task2 {
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.logging.FileHandler;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
+public class Task2 {
     public static void main(String[] args) {
+        Logger logger = Logger.getLogger(Task2.class.getName());
+        FileHandler fh;
+        try {  
+            fh = new FileHandler("MyLogFile.log");  
+            logger.addHandler(fh);
+            SimpleFormatter formatter = new SimpleFormatter();  
+            fh.setFormatter(formatter);  
+            logger.info("My first log");
+            } catch (SecurityException e) {  
+                e.printStackTrace();  
+            } catch (IOException e) {  
+                e.printStackTrace();  
+            }
+
         int arr[] = {9,3,2,5,1,7,6,4,8};
         int i = arr.length - 1;
         while(i >= 1)
@@ -12,6 +31,7 @@ public class Task2 {
             while (j < i) {
                 int temp;
                 if(arr[j+1] < arr[j]) {temp = arr[j]; arr[j] = arr[j+1]; arr[j+1] = temp;}
+                logger.info(Arrays.toString(arr));
                 j++;
                 }
             i--;
