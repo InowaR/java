d386f79166b01d6769e7304090c5fd191730ff90
georgiy <georgii_sidenko@mail.ru>
2023-06-13 22:04:19 +0300

    add 3 lesson

--- lesson3/src/App.java
+++ lesson3/src/App.java
@@ -53,10 +53,38 @@ public class App {
         System.out.println(min);
     }
 
+    public static void find_max(List<Integer> array) {
+        List<Integer> new_arr = array;
+        Iterator<Integer> itr = new_arr.iterator();
+        int max = itr.next();
+        while(itr.hasNext()) {
+            int i = itr.next();
+            if(i > max) {
+                max = i;
+            }
+        }
+        System.out.println(max);
+    }
+
+    public static void mean(List<Integer> array) {
+        List<Integer> new_arr = array;
+        Iterator<Integer> itr = new_arr.iterator();
+        int sum = 0;
+        int len = 0;
+        while(itr.hasNext()) {
+            int i = itr.next();
+            sum += i;
+            len++;
+        }
+        System.out.println(sum/len);
+    }
+
     public static void main(String[] args) throws Exception {
         List<Integer> array = create_array(10);
         print_array(array);
         delete_even_numbers(array);
         find_min(array);
+        find_max(array);
+        mean(array);
     }
 }
