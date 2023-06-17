2115395541f64e728a1f035976471edb049ffbbe
georgiy <georgii_sidenko@mail.ru>
2023-06-17 17:51:08 +0300

    add Deque

---
+++ lesson4/src/App.java
@@ -0,0 +1,32 @@
+// Задание
+
+// Даны два Deque, представляющие два целых числа. 
+// Цифры хранятся в обратном порядке и каждый из их узлов содержит одну цифру.
+// 1) Умножьте два числа и верните произведение в виде связанного списка.
+// 2) Сложите два числа и верните сумму в виде связанного списка.
+// Одно или два числа могут быть отрицательными.
+
+// Даны два Deque, цифры в обратном порядке.
+// [3,2,1,-] - пример Deque
+// [5,4,3]- пример второго Deque
+// 1) -123 * 345 = -42 435
+// Ответ всегда - связный список, в обычном порядке
+// [-,4,2,4,3,5] - пример ответа
+
+import java.util.ArrayDeque;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        ArrayDeque<String> firstDeque = new ArrayDeque<String>();
+        ArrayDeque<String> secondDeque = new ArrayDeque<String>();
+        firstDeque.add("3");
+        firstDeque.add("2");
+        firstDeque.add("1");
+        firstDeque.add("-");
+        secondDeque.add("5");
+        secondDeque.add("4");
+        secondDeque.add("4");
+        System.out.println(firstDeque);
+        System.out.println(secondDeque);
+    }
+}
